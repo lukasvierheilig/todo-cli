0d58eb6495828e073dad679fffbf9f9de77cde63
Lukas Vierheilig <lukas.vierheilig@gmail.com>
2024-09-07 10:16:14 +0200

    Repo init

---
+++ src/main/java/dev/lukas/todocli/TodoCliApplication.java
@@ -0,0 +1,13 @@
+package dev.lukas.todocli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoCliApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoCliApplication.class, args);
+    }
+
+}
