da914b74e1f89c8d12be2ad0e07e3ec308ed81e3
Lukas Vierheilig <lukas.vierheilig@gmail.com>
2024-09-08 13:07:44 +0200

    docs(todo): group todo commands in help command

--- src/main/java/dev/lukas/todocli/todo/commands/TodoCommands.java
+++ src/main/java/dev/lukas/todocli/todo/commands/TodoCommands.java
@@ -1,4 +1,8 @@
 package dev.lukas.todocli.todo.commands;
 
+import dev.lukas.todocli.todo.model.Todo;
+import org.springframework.shell.command.annotation.Command;
+import org.springframework.shell.command.annotation.Option;
+@Command(group = "Todo Commands", description = "Command managing todos.")
 public class TodoCommands {
 }
