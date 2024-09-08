a3a42da8f7fd29871e8c0b458c3e81086cc3e4ed
Lukas Vierheilig <lukas.vierheilig@gmail.com>
2024-09-08 13:00:26 +0200

    feat(todo): scan for commands in TodoCommands.class

--- src/main/java/dev/lukas/todocli/TodoCliApplication.java
+++ src/main/java/dev/lukas/todocli/TodoCliApplication.java
@@ -1,9 +1,12 @@
 package dev.lukas.todocli;
 
+import dev.lukas.todocli.todo.commands.TodoCommands;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.shell.command.annotation.EnableCommand;
 
 @SpringBootApplication
+@EnableCommand(TodoCommands.class)
 public class TodoCliApplication {
 
     public static void main(String[] args) {
