57f8a4d8ae48957bf7f47fbfaa1c2a0097699eff
Lukas Vierheilig <lukas.vierheilig@gmail.com>
2024-09-08 19:44:53 +0200

    feat(todo): rewrite reading and writing todos using apache commons csv

--- src/main/java/dev/lukas/todocli/todo/service/TodoService.java
+++ src/main/java/dev/lukas/todocli/todo/service/TodoService.java
@@ -1,16 +1,16 @@
 package dev.lukas.todocli.todo.service;
 
 import dev.lukas.todocli.todo.model.Todo;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVPrinter;
 import org.springframework.stereotype.Service;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
@@ -20,78 +20,19 @@ public class TodoService {
 
     private static final String CSV_FILE_NAME = ".todo/data/todos.csv";
     private static final String SYSTEM_PROPERTY_HOME = "user.home";
+    private static final String HEADER_ID = "id";
+    private static final String HEADER_CONTENT = "content";
+    private static final String HEADER_COMPLETED = "completed";
+    private static final String[] HEADERS = {HEADER_ID, HEADER_CONTENT, HEADER_COMPLETED};
 
-    private final File csvDataStore;
+    private final Path csvDataStorePath;
 
     public TodoService() {
-        this.csvDataStore = initDataFile();
-    }
-
-    private File initDataFile() {
-        File csvDataFile = resolveDataStorePath().toFile();
-        if (!csvDataFile.exists()) {
-            return createDataStoreFile(csvDataFile);
-        }
-        return csvDataFile;
-    }
-
-    private Path resolveDataStorePath() {
-        String homeDirectory = System.getProperty(SYSTEM_PROPERTY_HOME);
-        if (Objects.isNull(homeDirectory)) {
-            throw new IllegalStateException("System property '" + SYSTEM_PROPERTY_HOME + "' is not set");
-        }
-        return Path.of(homeDirectory, CSV_FILE_NAME).toAbsolutePath().normalize();
-    }
-
-    private File createDataStoreFile(File csvDataFile) {
-        File parentFile = csvDataFile.getParentFile();
-        if (Objects.isNull(parentFile)) {
-            throw new IllegalStateException("Could not get parent directory from data store file");
-        }
-        if (!parentFile.mkdirs()) {
-            throw new IllegalStateException("Could not create directory to data store file");
-        }
-        try {
-            if (!csvDataFile.createNewFile()) {
-                throw new IllegalStateException("Could not create file to data store file");
-            }
-            return csvDataFile;
-        } catch (IOException e) {
-            throw new IllegalStateException("Exception while creating data store file", e);
-        }
-    }
-
-    private void writeData(Todo data, File csvDataFile) {
-        assert csvDataStore != null && csvDataStore.exists() : "Csv data store does not exist";
-        assert data != null : "Data must not be null";
-        try (PrintWriter writer = new PrintWriter(new FileOutputStream(csvDataFile, true))) {
-            writer.println(data.toCsvDataRow());
-        } catch (FileNotFoundException e) {
-            throw new IllegalArgumentException("Could not write to data store. File not found.", e);
-        }
-    }
-
-    public void add(String newTodoContent) {
-        Todo data = new Todo(generateNextId(), newTodoContent, false);
-        writeData(data, this.csvDataStore);
-    }
-
-    private List<Todo> readData(File csvDataStore) {
-        assert csvDataStore != null && csvDataStore.exists() : "Csv data store does not exist";
-
-        try (BufferedReader reader = Files.newBufferedReader(csvDataStore.toPath())) {
-            return reader.lines()
-                    .filter(Objects::nonNull)
-                    .filter(line -> !line.isBlank())
-                    .map(line -> line.split(","))
-                    .map(Todo::fromCsvRow).toList();
-        } catch (IOException e) {
-            throw new IllegalStateException("Could not read from data store.", e);
-        }
+        this.csvDataStorePath = resolveDataStorePath();
     }
 
     public List<Todo> showAll() {
-        return readData(this.csvDataStore);
+        return readData();
     }
 
     public Optional<Todo> show(Long id) {
@@ -101,13 +42,85 @@ public class TodoService {
         if (id < 0) {
             throw new IllegalArgumentException("Id must not be negative");
         }
-        List<Todo> data = this.readData(this.csvDataStore);
+        List<Todo> data = this.readData();
         return data.stream()
                 .filter(todo -> todo.id() == id)
                 .findFirst();
     }
 
+    public void add(String newTodoContent) {
+        if (newTodoContent.isBlank()) {
+            throw new IllegalArgumentException("Content must not be blank");
+        }
+        Todo data = new Todo(generateNextId(), newTodoContent, false);
+        writeData(data);
+    }
+
+    private Path resolveDataStorePath() {
+        String homeDirectory = System.getProperty(SYSTEM_PROPERTY_HOME);
+        if (Objects.isNull(homeDirectory) || homeDirectory.isBlank()) {
+            throw new IllegalStateException("System property '" + SYSTEM_PROPERTY_HOME + "' is not set");
+        }
+        return Path.of(homeDirectory, CSV_FILE_NAME).toAbsolutePath().normalize();
+    }
+
+    private void writeData(Todo data) {
+        assert data != null : "Data must not be null";
+        assert csvDataStorePath != null : "Csv data path must not be null";
+
+        CSVFormat csvFormat;
+        StandardOpenOption[] options;
+
+        if (!csvDataStorePath.toFile().exists()) {
+            options = new StandardOpenOption[]{StandardOpenOption.CREATE_NEW};
+            csvFormat = CSVFormat.DEFAULT.builder()
+                    .setHeader(HEADERS)
+                    .build();
+
+        } else {
+            options = new StandardOpenOption[]{StandardOpenOption.APPEND};
+            csvFormat = CSVFormat.DEFAULT.builder()
+                    .setHeader(HEADERS)
+                    .setSkipHeaderRecord(true)
+                    .build();
+        }
+
+        try (CSVPrinter csvPrinter = new CSVPrinter(
+                Files.newBufferedWriter(
+                        csvDataStorePath,
+                        options),
+                csvFormat)) {
+            csvPrinter.printRecord(data.id(), data.contend(), data.completed());
+            csvPrinter.flush();
+        } catch (IOException e) {
+            throw new IllegalStateException("Could not read from data store.", e);
+        }
+    }
+
+    private List<Todo> readData() {
+        if (csvDataStorePath == null || !csvDataStorePath.toFile().exists()) {
+            return Collections.emptyList();
+        }
+
+        CSVFormat csvFormat = CSVFormat.DEFAULT.builder()
+                .setHeader(HEADERS)
+                .setSkipHeaderRecord(true)
+                .build();
+
+        try (CSVParser csvParser = csvFormat.parse(Files.newBufferedReader(csvDataStorePath))) {
+            return csvParser.getRecords().stream()
+                    .map(csvRecordRow -> new String[]{
+                            csvRecordRow.get(HEADER_ID),
+                            csvRecordRow.get(HEADER_CONTENT),
+                            csvRecordRow.get(HEADER_COMPLETED)})
+                    .map(Todo::fromCsvRow)
+                    .toList();
+        } catch (IOException e) {
+            throw new IllegalStateException("Could not read from data store.", e);
+        }
+    }
+
     private long generateNextId() {
-        return this.readData(this.csvDataStore).size();
+        return this.readData().size();
     }
 }
